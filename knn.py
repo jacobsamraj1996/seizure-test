2e10062d6a9175753618293232f2ca2fda72e0a8
jacobsamraj1996 <97592103+jacobsamraj1996@users.noreply.github.com>
2023-06-01 12:41:18 +0530

    Few changes

--- knn.py
+++ knn.py
@@ -8,12 +8,11 @@ from keras.layers import Dense
 ESR = pd.read_csv('data.csv')
 ESR.head()
 
-print(ESR.head()
-)
+print(ESR.head())
 
 cols = ESR.columns
 tgt = ESR.y
-tgt[tgt>1]=0
+tgt[tgt>1] = 0
 ax = sn.countplot(tgt,label="Count")
 non_seizure, seizure = tgt.value_counts()
 print('The number of trials for the non-seizure class is:', non_seizure)
@@ -24,6 +23,7 @@ print(ESR.isnull().sum())
 ESR.info()
 ESR.describe()
 print(ESR.describe())
+
 X = ESR.iloc[:,1:179].values
 X.shape
 print(X.shape)
@@ -41,101 +41,86 @@ plt.plot(X[0,:])
 plt.subplot(515)
 plt.plot(X[2,:])
 plt.xlabel('Samples')
-print(plt.xlabel('Samples'),plt.subplot(511),
-plt.plot(X[1,:]),
-plt.title('Classes'),
-plt.ylabel('uV'),
-plt.subplot(512),
-plt.plot(X[7,:]),
-plt.subplot(513),
-plt.plot(X[12,:]),
-plt.subplot(514),
-plt.plot(X[0,:]),
-plt.subplot(515),
-plt.plot(X[2,:]))
-
+print(plt.xlabel('Samples'), plt.subplot(511), plt.plot(X[1,:]), plt.title('Classes'), plt.ylabel('uV'), plt.subplot(512), plt.plot(X[7,:]), plt.subplot(513), plt.plot(X[12,:]), plt.subplot(514), plt.plot(X[0,:]), plt.subplot(515), plt.plot(X[2,:]))
 
 y = ESR.iloc[:,179].values
 y
 print(y)
-y[y>1]=0
+y[y>1] = 0
 y
+
 from sklearn.model_selection import train_test_split, cross_val_score
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
 
 from sklearn.preprocessing import StandardScaler
 sc = StandardScaler()
 X_train = sc.fit_transform(X_train)
 X_test = sc.transform(X_test)
 
-#Logistic Regression
+# Logistic Regression
 from sklearn.linear_model import LogisticRegression
 clf = LogisticRegression()
 clf.fit(X_train, y_train)
 y_pred_log_reg = clf.predict(X_test)
 acc_log_reg = round(clf.score(X_train, y_train) * 100, 2)
-print (str(acc_log_reg) + ' %')
+print(str(acc_log_reg) + ' %')
 
-#support vector machine(SVM)
+# Support Vector Machine (SVM)
 from sklearn.svm import SVC
 clf = SVC()
 clf.fit(X_train, y_train)
 y_pred_svc = clf.predict(X_test)
 acc_svc = round(clf.score(X_train, y_train) * 100, 2)
-print (str(acc_svc) + '%')
+print(str(acc_svc) + '%')
 
-#linear svm
-from sklearn.svm import SVC, LinearSVC
+# Linear SVM
+from sklearn.svm import LinearSVC
 clf = LinearSVC()
 clf.fit(X_train, y_train)
 y_pred_linear_svc = clf.predict(X_test)
 acc_linear_svc = round(clf.score(X_train, y_train) * 100, 2)
-print (str(acc_linear_svc) + '%')
+print(str(acc_linear_svc) + '%')
 
-#k-nearest neighbour
+# K-nearest neighbor
 from sklearn.neighbors import KNeighborsClassifier
 clf = KNeighborsClassifier()
 clf.fit(X_train, y_train)
 y_pred_knn = clf.predict(X_test)
 acc_knn = round(clf.score(X_train, y_train) * 100, 2)
-print (str(acc_knn)+'%')
+print(str(acc_knn) + '%')
 
-#Gaussian naive bayes
+# Gaussian Naive Bayes
 from sklearn.naive_bayes import GaussianNB
 clf = GaussianNB()
 clf.fit(X_train, y_train)
 y_pred_gnb = clf.predict(X_test)
 acc_gnb = round(clf.score(X_train, y_train) * 100, 2)
-print (str(acc_gnb) + '%')
+print(str(acc_gnb) + '%')
 
-# #artificial neural network
-# classifier = Sequential()
-# classifier.add(Dense(output_dim = 80, init = 'uniform', activation = 'relu', input_dim = 178))
-#
-# classifier.add(Dense(output_dim = 80, init = 'uniform', activation = 'relu'))
-# classifier.add(Dense(output_dim = 1, init = 'uniform', activation = 'sigmoid'))
-# classifier.compile(optimizer = 'adam', loss = 'binary_crossentropy', metrics = ['accuracy'])
+# Artificial Neural Network
+classifier = Sequential()
+classifier.add(Dense(units=80, kernel_initializer='uniform', activation='relu', input_dim=178))
+classifier.add(Dense(units=80, kernel_initializer='uniform', activation='relu'))
+classifier.add(Dense(units=1, kernel_initializer='uniform', activation='sigmoid'))
+classifier.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+classifier.fit(X_train, y_train, batch_size=10, epochs=100)
 
-#Fitting the ANN to the training set
-# classifier.fit(X_train, y_train, batch_size = 10, nb_epoch = 100)
-# # Predicting the Test set results
-# y_pred = classifier.predict(X_test)
-# y_pred = (y_pred > 0.5)
-# acc_ANN = round(clf.score(X_train, y_train) * 100, 2)
-# print (str(acc_ANN) + '%')
+# Predicting the Test set results
+y_pred = classifier.predict(X_test)
+y_pred = (y_pred > 0.5)
+acc_ANN = round(clf.score(X_train, y_train) * 100, 2)
+print(str(acc_ANN) + '%')
 
 from sklearn.decomposition import PCA
 pca = PCA()
 X_train_pca = pca.fit_transform(X_train)
 X_test_pca = pca.transform(X_test)
-acc_PCA = round(pca.score(X_train, y_train) )
-print (str(acc_PCA) + '%')
-models = pd.DataFrame({
-    'Model': ['Logistic Regression', 'Support Vector Machines', 'ANN',
-              'KNN', 'Naive Bayes', 'Principal Component Analysis'],
+acc_PCA = round(pca.score(X_train, y_train))
+print(str(acc_PCA) + '%')
 
-    'Score': [acc_log_reg, acc_svc,
-              acc_knn, acc_gnb, acc_PCA]
+models = pd.DataFrame({
+    'Model': ['Logistic Regression', 'Support Vector Machines', 'ANN', 'KNN', 'Naive Bayes', 'Principal Component Analysis'],
+    'Score': [acc_log_reg, acc_svc, acc_knn, acc_gnb, acc_ANN, acc_PCA]
 })
 
 models.sort_values(by='Score', ascending=False)
